24cccb22d647722a3d59e7cad918ea3c2a8f57f4
Luiza <luuh.nrodrigues@gmail.com>
2025-08-09 00:53:42 -0300

    meu primeiro commit

---
+++ dio.me/index.js
@@ -0,0 +1,23 @@
+let nome = "Luiza";
+let xp = 20000;
+let nivel;
+
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log(`A Heroína de nome ${nome} está no nível de ${nivel}`);
